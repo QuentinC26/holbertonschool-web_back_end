80375cc1725b1d7413e902759bd7f56c244e1677
QuentinC26 <q.zuzlewski@gmail.com>
2025-07-28 12:06:30 +0200

    i add the annotation and return multiplier

---
+++ python_variable_annotations/8-make_multiplier.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+add a annotation for function with callable object
+"""
+from typing import Callable
+
+
+def make_multiplier(multiplier: float) -> Callable[[float], float]:
+    """
+    add a annotation for function with object callable
+    """
+    return multiplier

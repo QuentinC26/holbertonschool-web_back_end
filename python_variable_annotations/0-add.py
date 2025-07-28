e7548e673a435a752226b4cd2a8e708dcba53a9b
QuentinC26 <q.zuzlewski@gmail.com>
2025-07-28 09:46:18 +0200

    i add annotation for a + b function

---
+++ python_variable_annotations/0-add.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+'''
+add a annotation for sum two number
+'''
+
+
+def add(a: float, b: float) -> float:
+    '''
+    add a annotation for sum two number
+    '''
+    return a + b

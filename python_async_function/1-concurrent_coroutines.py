47e233e18e1b1d1b55729b81e05eb7f184fcd364
QuentinC26 <q.zuzlewski@gmail.com>
2025-07-28 17:17:06 +0200

    i correct the indentation

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -1,13 +1,15 @@
 #!/usr/bin/env python3
-'''
+"""
 You will spawn wait_random n times with the specified max_delay.
-'''
+"""
 import asyncio
 import random
-wait_random = __import__('0-basic_async_syntax').wait_random
+
+wait_random = __import__("0-basic_async_syntax").wait_random
+
 
 async def wait_n(n: int, max_delay: int) -> list[float]:
-    '''
+    """
     You will spawn wait_random n times with the specified max_delay.
-    '''
+    """
     return await asyncio.gather(wait_random(max_delay), wait_n(n, max_delay))

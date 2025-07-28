9fdb0f0410bd390a5afe97383cc7a6b6a0f1a59d
QuentinC26 <q.zuzlewski@gmail.com>
2025-07-28 10:33:58 +0200

    i add function with list and annotation for sum list

---
+++ python_variable_annotations/5-sum_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+add a annotation for function with list
+"""
+
+
+def sum_list(input_list: float) -> float:
+    """
+    add a annotation for function with list
+    """
+    return sum(input_list)

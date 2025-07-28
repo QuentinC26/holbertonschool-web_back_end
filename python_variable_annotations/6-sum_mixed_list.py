06d53c73898845eae7fa506d2a5fbe6b2a552be8
QuentinC26 <q.zuzlewski@gmail.com>
2025-07-28 11:03:13 +0200

    i copy the code of task 5 and i add Union in the code

---
+++ python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+add a annotation for function with list
+"""
+from typing import Union, List
+
+
+def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
+    """
+    add a annotation for function with list
+    """
+    return sum(mxd_lst)

05852e1051c931cbe0d9711bf4e10d54c11d61ee
QuentinC26 <q.zuzlewski@gmail.com>
2025-07-28 17:16:32 +0200

    i change the comment

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 '''
-Write an asynchronous coroutine that takes in an integer argument
+You will spawn wait_random n times with the specified max_delay.
 '''
 import asyncio
 import random
@@ -8,6 +8,6 @@ wait_random = __import__('0-basic_async_syntax').wait_random
 
 async def wait_n(n: int, max_delay: int) -> list[float]:
     '''
-    Write an asynchronous coroutine that takes in an integer argument
+    You will spawn wait_random n times with the specified max_delay.
     '''
     return await asyncio.gather(wait_random(max_delay), wait_n(n, max_delay))

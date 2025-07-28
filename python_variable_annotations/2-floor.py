cc5c9e39d6925c715427240488a6becedd544248
QuentinC26 <q.zuzlewski@gmail.com>
2025-07-28 10:01:51 +0200

    i add return math.floor(n) with annotations

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+'''
+add a annotation for concat two string
+'''
+import math
+
+
+def floor(n: float) -> int:
+    '''
+    add a annotation for concat two string
+    '''
+    return  math.floor(n)

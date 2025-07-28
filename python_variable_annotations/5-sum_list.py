99127a6537b1bb6c12f8e69a1fe74f91995cf0d2
QuentinC26 <q.zuzlewski@gmail.com>
2025-07-28 10:51:28 +0200

    i add typing module and i replace list[float] by List[float]

--- python_variable_annotations/5-sum_list.py
+++ python_variable_annotations/5-sum_list.py
@@ -2,9 +2,10 @@
 """
 add a annotation for function with list
 """
+from typing import List
 
 
-def sum_list(input_list: list[float]) -> float:
+def sum_list(input_list: List[float]) -> float:
     """
     add a annotation for function with list
     """

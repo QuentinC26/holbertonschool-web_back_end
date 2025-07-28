9466b79e970821844d8b2fab9d639eec5134d1a2
QuentinC26 <q.zuzlewski@gmail.com>
2025-07-28 09:51:43 +0200

    i add function for concat str1 and str2 and i add anotations

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+'''
+add a annotation for concat two string
+'''
+
+
+def concat(str1: str, str2: str) -> str:
+    '''
+    add a annotation for concat two string
+    '''
+    return str1 + str2

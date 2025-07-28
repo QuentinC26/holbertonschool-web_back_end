932de2b14c592b3264b7268215e4b4c271870662
QuentinC26 <q.zuzlewski@gmail.com>
2025-07-28 10:17:14 +0200

    i add f string for have the good result

---
+++ python_variable_annotations/3-to_str.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+add a annotation for returns the string representation of the float
+"""
+
+
+def to_str(n: float) -> str:
+    """
+    add a annotation for returns the string representation of the float
+    """
+    return f"{n}"

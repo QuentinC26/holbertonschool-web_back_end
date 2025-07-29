64cd952a7a4dba59faeeb9cf22cd553e08730068
QuentinC26 <q.zuzlewski@gmail.com>
2025-07-29 11:03:04 +0200

    i add import for resolve problem of points validation

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -3,12 +3,13 @@
 You will spawn wait_random n times with the specified max_delay.
 """
 import asyncio
+from typing import List
 import random
 
 wait_random = __import__("0-basic_async_syntax").wait_random
 
 
-async def wait_n(n: int, max_delay: int) -> list[float]:
+async def wait_n(n: int, max_delay: int) -> List[float]:
     """
     You will spawn wait_random n times with the specified max_delay.
     """

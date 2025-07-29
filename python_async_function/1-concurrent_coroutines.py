9d5d3105d6bd729c89674b064ffe7e941274fcda
QuentinC26 <q.zuzlewski@gmail.com>
2025-07-29 09:39:33 +0200

    i add new_list, for loop with create_task function and gather function. I use sorted in the return

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -12,4 +12,9 @@ async def wait_n(n: int, max_delay: int) -> list[float]:
     """
     You will spawn wait_random n times with the specified max_delay.
     """
-    return await asyncio.gather(wait_random(max_delay), wait_n(n, max_delay))
+    multiple_wait_random = []
+    for index in range(n):
+        create_task = asyncio.create_task(wait_random(max_delay))
+        multiple_wait_random.append(create_task)
+    result = await asyncio.gather(*multiple_wait_random)
+    return sorted(result)

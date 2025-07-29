143d5cf1bd98ff6319ae15b8b7a02a93ea54a75c
QuentinC26 <q.zuzlewski@gmail.com>
2025-07-29 13:28:14 +0200

    i remove wait_n function and i remove the bubble sort

--- python_async_function/4-tasks.py
+++ python_async_function/4-tasks.py
@@ -10,7 +10,7 @@ wait_random = __import__("0-basic_async_syntax").wait_random
 wait_random = __import__("3-tasks").wait_random
 
 
-async def wait_n(n: int, max_delay: int) -> List[float]:
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
     """
     You will spawn wait_random n times with the specified max_delay.
     """
@@ -20,29 +20,5 @@ async def wait_n(n: int, max_delay: int) -> List[float]:
         multiple_wait_random.append(create_task)
     # end = result
     # i use end for respect pycodestyle restriction
-    end = await asyncio.gather(*multiple_wait_random)
-    lens = len(end)
-    for size in range(lens-1):
-        for index in range(lens-size-1):
-            if end[index] > end[index + 1]:
-                end[index], end[index + 1] = end[index + 1], end[index]
-    return end
-
-
-async def task_wait_n(n: int, max_delay: int) -> List[float]:
-    """
-    You will spawn wait_random n times with the specified max_delay.
-    """
-    multiple_task_wait_random = []
-    for index in range(n):
-        create_task = asyncio.create_task(task_wait_random(max_delay))
-        multiple_task_wait_random.append(create_task)
-    # end = result
-    # i use end for respect pycodestyle restriction
-    end = await asyncio.gather(*multiple_wait_random)
-    lens = len(end)
-    for size in range(lens-1):
-        for index in range(lens-size-1):
-            if end[index] > end[index + 1]:
-                end[index], end[index + 1] = end[index + 1], end[index]
-    return end
+    result = await asyncio.gather(*multiple_wait_random)
+    return sorted(result)

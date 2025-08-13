474ca1a76a9b180d3c64940b5ea9f725a3f97bef
QuentinC26 <q.zuzlewski@gmail.com>
2025-08-13 12:03:38 +0200

    i remove the index_range function outside the class

--- pagination/1-simple_pagination.py
+++ pagination/1-simple_pagination.py
@@ -7,6 +7,15 @@ import math
 from typing import List
 
 
+def index_range(page, page_size):
+    '''
+    Write a function named index_range that takes two integer arguments
+    '''
+    start_index = (page - 1) * page_size
+    end_index = start_index + page_size
+    return (start_index, end_index)
+
+
 class Server:
     """
     Server class to paginate a database of popular baby names.
@@ -26,18 +35,10 @@ class Server:
             self.__dataset = dataset[1:]
         return self.__dataset
 
-    def index_range(self, page, page_size):
-        '''
-        Write a function named index_range that takes two integer arguments
-        '''
-        start_index = (page - 1) * page_size
-        end_index = start_index + page_size
-        return (start_index, end_index)
-
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
         assert isinstance(page, int) and isinstance(page_size, int)
         assert page > 0 and page_size > 0
-        start_index, end_index = self.index_range(page, page_size)
+        start_index, end_index = index_range(page, page_size)
         if start_index > len(self.dataset()) or end_index > len(self.dataset()):
             return []
         return self.dataset()[start_index:end_index]

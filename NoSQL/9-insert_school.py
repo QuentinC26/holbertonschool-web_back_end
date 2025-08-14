f18f9a8e75044ad4c168182b9fd0acd5150d3e42
QuentinC26 <q.zuzlewski@gmail.com>
2025-08-14 11:21:00 +0200

    add insert_one method

---
+++ NoSQL/9-insert_school.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+inserts a new document in a collection based on kwargs
+"""
+
+
+def insert_school(mongo_collection, **kwargs):
+    """
+    inserts a new document in a collection based on kwargs
+    """
+    insert_new_element = mongo_collection.insert_one(kwargs)
+    return insert_new_element

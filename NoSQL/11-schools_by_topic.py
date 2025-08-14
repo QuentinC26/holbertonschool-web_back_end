d37ad2ddfa22e7696b97eaf34c0ec34b4d3616f2
QuentinC26 <q.zuzlewski@gmail.com>
2025-08-14 15:09:37 +0200

    i replace topics by topic

--- NoSQL/11-schools_by_topic.py
+++ NoSQL/11-schools_by_topic.py
@@ -8,5 +8,5 @@ def schools_by_topic(mongo_collection, topic):
     """
     returns the list of school having a specific topic
     """
-    list_of_topic = mongo_collection.find({"topic": topic})
+    list_of_topic = mongo_collection.find({"topics": topic})
     return list(list_of_topic)

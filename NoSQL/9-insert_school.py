c2a3e413b5cd3a70ec167da474438dfeb618ccf0
QuentinC26 <q.zuzlewski@gmail.com>
2025-08-14 13:29:16 +0200

    i add inserted_id in the return

--- NoSQL/9-insert_school.py
+++ NoSQL/9-insert_school.py
@@ -9,4 +9,4 @@ def insert_school(mongo_collection, **kwargs):
     inserts a new document in a collection based on kwargs
     """
     insert_new_element = mongo_collection.insert_one(kwargs)
-    return insert_new_element
+    return insert_new_element.inserted_id

e8579a92378245b30e1656fcd2be2ea96c3afa85
QuentinC26 <q.zuzlewski@gmail.com>
2025-08-15 18:50:34 +0200

    i add if name == main

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -4,14 +4,15 @@ provides some stats about Nginx logs stored in MongoDB
 """
 import pymongo
 
-logs = pymongo.MongoClient("mongodb://localhost:27017/")
-nginx = logs["logs"]["nginx"]
-number_of_documents = nginx.count_documents({})
-print(f"{number_of_documents} logs")
-print("Methods:")
-print("\tmethod GET:", nginx.count_documents({"method": "GET"}))
-print("\tmethod POST:", nginx.count_documents({"method": "POST"}))
-print("\tmethod PUT:", nginx.count_documents({"method": "PUT"}))
-print("\tmethod PATCH:", nginx.count_documents({"method": "PATCH"}))
-print("\tmethod DELETE:", nginx.count_documents({"method": "DELETE"}))
-print(nginx.count_documents({"method": "GET", "path": "/status"}), "status check")
+if __name__ == "__main__":
+    logs = pymongo.MongoClient("mongodb://localhost:27017/")
+    nginx = logs["logs"]["nginx"]
+    number_of_documents = nginx.count_documents({})
+    print(f"{number_of_documents} logs")
+    print("Methods:")
+    print("\tmethod GET:", nginx.count_documents({"method": "GET"}))
+    print("\tmethod POST:", nginx.count_documents({"method": "POST"}))
+    print("\tmethod PUT:", nginx.count_documents({"method": "PUT"}))
+    print("\tmethod PATCH:", nginx.count_documents({"method": "PATCH"}))
+    print("\tmethod DELETE:", nginx.count_documents({"method": "DELETE"}))
+    print(nginx.count_documents({"method": "GET", "path": "/status"}), "status check")

7b96920ad9cdcfc6da4d726c83d1c2591757e52e
QuentinC26 <q.zuzlewski@gmail.com>
2025-08-20 10:46:00 +0200

    i add two lines in the function and i remove the return in the loop

--- ES6_basic/4-rest-parameter.js
+++ ES6_basic/4-rest-parameter.js
@@ -1,6 +1,7 @@
 export default function returnHowManyArguments(...args) {
+  returnHowManyArguments = 0;
   for(let index = 0; index < args; index++) {
-    index = index + 1
-    return index
+    index = index + 1;
   }
+  return returnHowManyArguments;
 }

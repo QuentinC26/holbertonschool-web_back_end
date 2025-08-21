9444ce123c24ecec7f92b2c41b7d660ddb644226
QuentinC26 <q.zuzlewski@gmail.com>
2025-08-21 14:54:57 +0200

    i replace index by values and i use push in the for

--- ES6_basic/10-loops.js
+++ ES6_basic/10-loops.js
@@ -1,9 +1,7 @@
 export default function appendToEachArrayValue(array, appendString) {
   const thelist = [];
-  for (const index of array) {
-    thelist[0] = appendString + array[0];
-    thelist[1] = appendString + array[1];
-    thelist[2] = appendString + array[2];
+  for (const values of array) {
+    thelist.push(appendString + values)
   }
   return thelist;
 }

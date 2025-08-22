d7a04685499bfb285e122670e2d33715588a0b2c
QuentinC26 <q.zuzlewski@gmail.com>
2025-08-22 10:22:18 +0200

    i add the condition in the set name

--- ES6_classes/2-hbtn_course.js
+++ ES6_classes/2-hbtn_course.js
@@ -1,8 +1,5 @@
 export default class HolbertonCourse {
   constructor(name, length, students) {
-    if (typeof (name) !== 'string') {
-      throw new TypeError('Name must be a string');
-    }
     if (typeof (length) !== 'number') {
       throw new TypeError('Length must be a number');
     }
@@ -16,12 +13,15 @@ export default class HolbertonCourse {
     this._length = length;
     this._students = students;
   }
-  
+
   get name() {
     return this._name;
   }
 
   set name(name) {
+    if (typeof (name) !== 'string') {
+      throw new TypeError('Name must be a string');
+    }
     return this._name = name;
   }
 

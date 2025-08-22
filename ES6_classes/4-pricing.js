9d2732bfe0bf5bfa2f58e8f252adf276cc4fc1ce
QuentinC26 <q.zuzlewski@gmail.com>
2025-08-22 11:02:47 +0200

    i add constructor, getter, setter, displayFullPrice and convertPrice method

---
+++ ES6_classes/4-pricing.js
@@ -0,0 +1,26 @@
+import Currency from './3-currency';
+
+export default class Pricing {
+  constructor(amount, currency) {
+    this._amount = amount;
+    this._currency = currency;
+  }
+  get amount() {
+    return this._amount;
+  }
+  set amount(amount) {
+    return this._amount = amount;
+  }
+  get currency() {
+    return this._currency;
+  }
+  set currency(currency) {
+    return this._currency = currency;
+  }
+  displayFullPrice() {
+    return this._amount + ' ' + this.currency.name + ' ' + '(' + this.currency.code + ')';
+  }
+  convertPrice(amount, conversionRate) {
+    return amount * conversionRate;
+  }
+}

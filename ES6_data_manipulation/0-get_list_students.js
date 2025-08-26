9eb4432489513738fc387465d896bf27d47ad25d
QuentinC26 <q.zuzlewski@gmail.com>
2025-08-26 10:16:49 +0200

    i remove the paramater of the functions

--- ES6_data_manipulation/0-get_list_students.js
+++ ES6_data_manipulation/0-get_list_students.js
@@ -1,24 +1,8 @@
-export default function getListStudents(id, firstName, location) {
+export default function getListStudents() {
   const Array = [
-    {
-    'id': 1,
-    'firstname': 'Guillaume',
-    'location': 'San Francisco'
-    },
-    {
-    'id': 2,
-    'firstname': 'James',
-    'location': 'Columbia'
-    },
-    {
-    'id': 5,
-    'firstname': 'Serena',
-    'location': 'San Francisco'
-    }
+    { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
+    { id: 2, firstName: 'James', location: 'Columbia' },
+    { id: 5, firstName: 'Serena', location: 'San Francisco' }
   ]
-  id = Array[0];
-  firstName = Array[1];
-  location = Array[2];
-  const NewArray = [id, firstName, location]
-  return NewArray;
+  return Array;
 }

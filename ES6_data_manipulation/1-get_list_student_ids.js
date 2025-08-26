90b6109618f9ed28fbb9fb39acbfc7c47a4f0328
QuentinC26 <q.zuzlewski@gmail.com>
2025-08-26 11:17:31 +0200

    i move the map in the function and i remove all things outsite the function

--- ES6_data_manipulation/1-get_list_student_ids.js
+++ ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,11 +1,6 @@
-const NewArray = [
-  { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
-  { id: 2, firstName: 'James', location: 'Columbia' },
-  { id: 5, firstName: 'Serena', location: 'San Francisco' }
-]
-
-NewArray.map(getListStudentIds);
-
 export default function getListStudentIds(NewArray) {
-  return [NewArray.id];
+  if (!Array.isArray(NewArray)) {
+    return [];
+  }
+  return NewArray.map(NewId => NewId.id)
 }

4481868d0289a593f4ad0cd21fe47f951827258a
QuentinC26 <q.zuzlewski@gmail.com>
2025-08-28 13:27:55 +0200

    i modify the grade line

--- ES6_data_manipulation/4-update_grade_by_city.js
+++ ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,12 +1,11 @@
 export default function updateStudentGradeByCity(StudentsList, city, newGrades) {
   return StudentsList
-    .filter((Students) => Students.location === city)
-    .map((Students) => {
-      const Grades = newGrades.find((gde) => gde.studentId === StudentsList.id); 'gde = grade'
+    .filter((student) => student.location === city)
+    .map((student) => {
+      const Grades = newGrades.find((gde) => gde.studentId === student.id); //gde = grade
       return {
-        ...Students,
-        grade: Grades
-      }
-    }
-  )
+        ...student,
+        grade: Grades ? Grades.grade:'N/A',
+      };
+    });
 }

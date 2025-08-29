4e959d3e929647ae2f8a78a8331c4516a694a430
QuentinC26 <q.zuzlewski@gmail.com>
2025-08-29 18:15:02 +0200

    i add new promise and i add return thepromise.then

--- ES6_promise/1-promise.js
+++ ES6_promise/1-promise.js
@@ -1,15 +1,23 @@
 export default function getFullResponseFromAPI(success) {
+  let ThePromise = new Promise(function(Resolve, Reject) {
+  
   if (success === true) {
-    let Promise = {
-        status: 200,
-        body: 'Success'
-    };
-    return `Promise { ${JSON.stringify(Promise)} }`
-   } else {
-    console.log('Promise {')
-    return `  <rejected> Error: The fake API is not working currently
-    ...
-    ...
-    `
+    Resolve();
+  } else {
+    Reject();
   }
+  });
+
+  return ThePromise.then(
+    function(value) {
+      let NewDict = { 
+        status: 200, 
+        body: 'Success'
+      }
+      return NewDict;
+    },
+    function(error){
+      throw new Error('The fake API is not working currently')
+    }
+  )
 }

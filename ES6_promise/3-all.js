a7478932dd7ac5f5270bd1b3feff028f8e74f4e9
QuentinC26 <q.zuzlewski@gmail.com>
2025-08-29 23:10:39 +0200

    i add return promise.all

---
+++ ES6_promise/3-all.js
@@ -0,0 +1,5 @@
+import { createUser, uploadPhoto } from './utils.js';
+
+export default function handleProfileSignup() {
+  return Promise.all([uploadPhoto, createUser]).then((regroupall) => { regroupall })
+}

54cdf9f43d9286655b65851fea12c8735699a72b
QuentinC26 <q.zuzlewski@gmail.com>
2025-08-29 23:32:43 +0200

    i add console.log

--- ES6_promise/3-all.js
+++ ES6_promise/3-all.js
@@ -2,5 +2,6 @@ import { createUser, uploadPhoto } from './utils.js';
 
 export default function handleProfileSignup() {
   return Promise.all([uploadPhoto(), createUser()])
-  .then((regroupall) => { regroupall.body, regroupall.firstname, regroupall.lastname })
+  .then((regroupall) => { console.log(`${regroupall[0].body} ${regroupall[1].firstName} ${regroupall[1].lastName}`)})
+  .catch((error) => { console.log('Signup system offline')})
 }

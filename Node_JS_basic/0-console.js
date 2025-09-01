9bbee34df6b27f3f4290b3f075bf9e7d250f1eb1
QuentinC26 <q.zuzlewski@gmail.com>
2025-09-01 15:19:16 +0200

    i remove the text and i add console.log stdout

--- Node_JS_basic/0-console.js
+++ Node_JS_basic/0-console.js
@@ -1,9 +1,5 @@
-function displayMessage() {
-  const http = require('http');
-  http.createServer((req, res) => {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('Hello NodeJS!');
-  }).listen(8080);
+function displayMessage(STDOUT) {
+  console.log(STDOUT)
 }
 
 module.exports = displayMessage; 

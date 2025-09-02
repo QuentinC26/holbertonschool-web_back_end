a88d5fe5f7c21b56d9a9019e4080323c4ed0f3b0
QuentinC26 <q.zuzlewski@gmail.com>
2025-09-02 14:35:22 +0200

    i remove the process.exit and i replace by process.on and i replace the console log by process.stdout.write

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,7 +1,9 @@
 console.log('Welcome to Holberton School, what is your name?');
 
 process.stdin.on('data', (data) => {
-  console.log(`Your name is: ${data.toString()}`);
-  console.log('This important software is now closing');
-  process.exit();
+  process.stdout.write(`Your name is: ${data.toString()}`);
 });
+
+process.on('exit', () => {
+  process.stdout.write('This important software is now closing');
+})

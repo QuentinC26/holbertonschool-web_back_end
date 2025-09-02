16601d89e5924930ceee74a929f895c3955b1159
QuentinC26 <q.zuzlewski@gmail.com>
2025-09-02 13:36:59 +0200

    i add process stdin with two console.log

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,12 +1,7 @@
 console.log('Welcome to Holberton School, what is your name?');
 
-const args = process.argv;
-const name = args[2];
-
-console.log(name);
-
-console.log('Your name is:', name)
-
-process.on('exit', (code) => {
-   console.log('This important software is now closing');
+process.stdin.on('data', data => {
+console.log(`Your name is: ${data.toString()}`);
+console.log('This important software is now closing')
+process.exit();
 });

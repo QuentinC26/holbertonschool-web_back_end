f1e1cabb3ff6f3a54e4e9bff9e596a4dee17e33a
QuentinC26 <q.zuzlewski@gmail.com>
2025-09-02 14:47:19 +0200

    i add

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,9 +1,11 @@
 console.log('Welcome to Holberton School, what is your name?');
 
 process.stdin.on('data', (data) => {
-  process.stdout.write(`Your name is: ${data.toString()}`);
+  const name = data.toString().trim();
+  process.stdout.write(`Your name is: ${name}\n`);
+  process.exit();
 });
 
 process.on('exit', () => {
-  process.stdout.write('This important software is now closing');
+  console.log('This important software is now closing');
 });

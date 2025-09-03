c1c25f1119ba46b31779bd0da21d42a072c5bd7d
QuentinC26 <q.zuzlewski@gmail.com>
2025-09-03 17:06:16 +0200

    i resolve eslint problem

--- Node_JS_basic/2-read_file.js
+++ Node_JS_basic/2-read_file.js
@@ -1,6 +1,7 @@
+const fs = require('fs');
+
 function countStudents(path) {
   try {
-    const fs = require('fs');
     const data = fs.readFileSync(path, { encoding: 'utf8', flag: 'r' });
     const line = data.trim().split('\n');
     const FirstList = [];
@@ -10,20 +11,19 @@ function countStudents(path) {
       const separatorOne = line[index].split(',');
       FirstList.push(separatorOne);
     }
-    FirstList.shift()
+    FirstList.shift();
     for (const indexTwo in FirstList) {
       if (FirstList[indexTwo][3] === 'CS') {
         CSList.push(FirstList[indexTwo][0]);
-       }
-       if (FirstList[indexTwo][3] === 'SWE') {
+      }
+      if (FirstList[indexTwo][3] === 'SWE') {
         SWEList.push(FirstList[indexTwo][0]);
-       } 
+      }
     }
     console.log(`Number of students: ${(FirstList.length)}`);
     console.log(`Number of students in CS: ${(CSList.length)}. List: ${(CSList)}`);
     console.log(`Number of students in SWE: ${(SWEList.length)}. List: ${(SWEList)}`);
-  }
-  catch (err) {
+  } catch (err) {
     throw new Error('Cannot load the database');
   }
 }

2016ea8aad929912cff136d2c85fd2a8ba78d3fd
QuentinC26 <q.zuzlewski@gmail.com>
2025-09-03 10:09:11 +0200

    i add trim method

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,7 +1,7 @@
 console.log('Welcome to Holberton School, what is your name?');
 
 process.stdin.on('data', (data) => {
-  process.stdout.write(`Your name is: ${data.toString()}`);
+  process.stdout.write(`Your name is: ${data.toString()}`.trim());
 });
 
 process.on('exit', () => {

d4bd772181d71aafcda30f94c1efa9cb42349a4e
QuentinC26 <q.zuzlewski@gmail.com>
2025-09-03 14:24:33 +0200

    i resolve eslint problem

--- Node_JS_basic/2-read_file.js
+++ Node_JS_basic/2-read_file.js
@@ -2,29 +2,28 @@ function countStudents(path) {
   try {
     const fs = require('fs');
     const data = fs.readFileSync(path, { encoding: 'utf8', flag: 'r' });
-    const line = data.trim().split("\n");
-    TestList = [];
-    FinalList = [];
-    CSList = [];
-    SWEList = [];
-    for (index in line) {
-        const separatorOne = line[index].split(",");
-        TestList.push(separatorOne);
+    const line = data.trim().split('\n');
+    const FirstList = [];
+    const CSList = [];
+    const SWEList = [];
+    for (const index in line) {
+      const separatorOne = line[index].split(',');
+      FirstList.push(separatorOne);
     }
-    TestList.shift()
-    for (indexTwo in TestList) {
-       if (TestList[indexTwo][3] === 'CS') {
-          CSList.push(TestList[indexTwo][0]);
+    FirstList.shift()
+    for (const indexTwo in FirstList) {
+      if (FirstList[indexTwo][3] === 'CS') {
+        CSList.push(FirstList[indexTwo][0]);
        }
-       if (TestList[indexTwo][3] === 'SWE') {
-          SWEList.push(TestList[indexTwo][0]);
+       if (FirstList[indexTwo][3] === 'SWE') {
+        SWEList.push(FirstList[indexTwo][0]);
        } 
     }
-    console.log(`Number of students: ${(TestList.length)}`);
+    console.log(`Number of students: ${(FirstList.length)}`);
     console.log(`Number of students in CS: ${(CSList.length)}. List: ${(CSList)}`);
     console.log(`Number of students in SWE: ${(SWEList.length)}. List: ${(SWEList)}`);
   }
-  catch(err) {
+  catch (err) {
     throw new Error('Cannot load the database');
   }
 }

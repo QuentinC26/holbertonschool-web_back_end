4d230c8b2239362971fbf2075de15b5896bf7ff8
QuentinC26 <q.zuzlewski@gmail.com>
2025-09-05 21:46:05 +0200

    i add my object in the variable app and the module exports

--- Node_JS_basic/4-http.js
+++ Node_JS_basic/4-http.js
@@ -1,8 +1,10 @@
 const http = require('http');
 
-http.createServer((req, app) => {
-  app.writeHead(200, { 'Content-Type': 'text/plain' });
-  app.end('Hello Holberton School!');
+app = http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('Hello Holberton School!');
 })
 
   .listen(1245);
+
+module.exports = app;

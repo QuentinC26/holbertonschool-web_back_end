912dd5b54810ae9e0304b61a7a894fd2fb810b82
QuentinC26 <q.zuzlewski@gmail.com>
2025-09-28 23:20:26 +0200

    i chose the two option

--- ES6_promise/4-user-promise.js
+++ ES6_promise/4-user-promise.js
@@ -1,17 +1,19 @@
-export default function signUpUser(firstName, lastName) {
-  return new Promise(function(Resolve, Reject) { 
-    Resolve()
-    Reject()
-  })
-  .then((viewall) => {
-    return {
-        firstName,
-        lastName,
-    }
- })
-}
+
+//
+// export default function signUpUser(firstName, lastName) {
+//  return new Promise(function(Resolve, Reject) { 
+//    Resolve()
+//   Reject()
+//  })
+//  .then((viewall) => {
+//   return {
+//        firstName,
+//        lastName,
+//    }
+//  })
+// }
 
 // code plus adapté :
-// export default function signUpUser(firstName, lastName) {
-//  return Promise.resolve({firstName, lastName,})
-// }
+export default function signUpUser(firstName, lastName) {
+  return Promise.resolve({firstName, lastName,})
+}

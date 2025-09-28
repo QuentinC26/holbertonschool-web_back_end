2c56251df8bcef8f6eafc1cd1f83d4ad552a6e8b
QuentinC26 <q.zuzlewski@gmail.com>
2025-09-28 23:31:20 +0200

    i add then, map and the condition

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -2,8 +2,14 @@ import signUpUser from './4-user-promise.js';
 import uploadPhoto from './5-photo-reject.js';
 
 export default function handleProfileSignup(firstName, lastName, filename) {
-  return Promise.allSettled([
-    signUpUser(firstName, lastName),
-    uploadPhoto(filename)
-  ]);
+  return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(filename)]
+    .then(results =>
+      results.map(result => {
+        if (result.status === 'rejected') {
+          // Changer la clé 'reason' en 'value' pour le test
+          return { status: result.status, value: result.reason };
+        }
+        return result;
+      })
+  ));
 }

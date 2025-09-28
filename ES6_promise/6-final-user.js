05dd514fe17fe93aa70cec27df8fed9937832411
QuentinC26 <q.zuzlewski@gmail.com>
2025-09-28 23:36:19 +0200

    i add two import

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -1,3 +1,6 @@
+import signUpUser from './4-user-promise.js';
+import uploadPhoto from './5-photo-reject.js';
+
 export default async function handleProfileSignup(firstName, lastName, filename) {
   const results = await Promise.allSettled([
     signUpUser(firstName, lastName),

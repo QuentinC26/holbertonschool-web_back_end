3f8aed8bae3c803caf4a678f8cbbd7e45a4c54b1
QuentinC26 <q.zuzlewski@gmail.com>
2025-09-28 23:21:40 +0200

    i reverse signup and upload

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -2,5 +2,5 @@ import signUpUser from './4-user-promise.js';
 import uploadPhoto from './5-photo-reject.js';
 
 export default function handleProfileSignup(firstName, lastName, filename) {
-  return Promise.allSettled([uploadPhoto(filename), signUpUser(firstName, lastName)]);
+  return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(filename)]);
 }
